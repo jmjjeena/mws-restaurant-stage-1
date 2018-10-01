d96b3e1341b50f1e7ca0d90d7faa3692f1a628a7
Jeena <jmj.jeena@gmail.com>
2018-10-01 16:47:03 -0700

    Added maps with MapBox API

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoiam1qamVlbmEiLCJhIjoiY2ptcXg2Mjc3MDFibDN3b2RtYTN3NTVubiJ9.5JEPj6LB-txFj0djByIhHA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
