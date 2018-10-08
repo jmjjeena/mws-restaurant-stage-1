613d400544ab2a463f73aa7464d2b7cc08927cad
Jeena <jmj.jeena@gmail.com>
2018-10-07 19:56:08 -0700

    Installation of event to add cacheFiles.

--- sw.js
+++ sw.js
@@ -1,2 +1,30 @@
-console.log('service worker: registered');
+// console.log('service worker: registered');
+const cacheName = 'cache1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js'
+];
 
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cache1).then(function(cache) {
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
